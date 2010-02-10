a8c62cdbd1b9baf3e9c5a23a2551df146fd343ca
Remigijus Kiminas <remdex@gmail.com>
2010-02-10 18:45:34 +0000

    Etag caching enable/disable options

--- modules/lhgallery/lastcommented.php
+++ modules/lhgallery/lastcommented.php
@@ -3,20 +3,22 @@
 $cache = CSCacheAPC::getMem(); 
 $cacheKey = md5('version_'.$cache->getCacheVersion('last_commented').'lastcommented_view_url'.'_page_'.$Params['user_parameters_unordered']['page']);
     
-$ExpireTime = 3600;
-$currentKeyEtag = md5($cacheKey.'user_id_'.erLhcoreClassUser::instance()->getUserID());
-header('Cache-Control: max-age=' . $ExpireTime); // must-revalidate
-header('Expires: '.gmdate('D, d M Y H:i:s', time()+$ExpireTime).' GMT');
-header('ETag: ' . $currentKeyEtag); 
-
-$iftag = isset($_SERVER['HTTP_IF_NONE_MATCH']) ? $_SERVER['HTTP_IF_NONE_MATCH'] == $currentKeyEtag : null;         
-if ($iftag === true)
-{   
-    header ("HTTP/1.0 304 Not Modified");
-    header ('Content-Length: 0');
-    exit;
+if (erConfigClassLhConfig::getInstance()->conf->getSetting( 'site', 'etag_caching_enabled' ) === true)
+{
+    $ExpireTime = 3600;
+    $currentKeyEtag = md5($cacheKey.'user_id_'.erLhcoreClassUser::instance()->getUserID());
+    header('Cache-Control: max-age=' . $ExpireTime); // must-revalidate
+    header('Expires: '.gmdate('D, d M Y H:i:s', time()+$ExpireTime).' GMT');
+    header('ETag: ' . $currentKeyEtag); 
+    
+    $iftag = isset($_SERVER['HTTP_IF_NONE_MATCH']) ? $_SERVER['HTTP_IF_NONE_MATCH'] == $currentKeyEtag : null;         
+    if ($iftag === true)
+    {   
+        header ("HTTP/1.0 304 Not Modified");
+        header ('Content-Length: 0');
+        exit;
+    }
 }
- 
 if (($Result = $cache->restore($cacheKey)) === false)
 {    
     $tpl = erLhcoreClassTemplate::getInstance( 'lhgallery/lastcommented.tpl.php');
