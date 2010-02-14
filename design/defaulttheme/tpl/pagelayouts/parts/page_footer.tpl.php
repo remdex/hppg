bec88cd71239dd7cfc4a7bd8dcc4a121fe92f3ac
Remigijus Kiminas <remdex@gmail.com>
2010-02-14 17:06:21 +0000

    Removed analytics :D

--- design/defaulttheme/tpl/pagelayouts/parts/page_footer.tpl.php
+++ design/defaulttheme/tpl/pagelayouts/parts/page_footer.tpl.php
@@ -2,12 +2,3 @@
     <div class="right"><acronym title="It's NOT fake!">Rendered in: <?=number_format(set_time($GLOBALS['star_microtile'], microtime()), 5);?> s.</acronym>, powered by <a href="http://fof.remdex.info">http://fof.remdex.info</a></div>
 	<div class="creator copyright">&copy; <?=date('Y')?></div>
 </div>
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-1221542-6");
-pageTracker._trackPageview();
-} catch(err) {}</script>
