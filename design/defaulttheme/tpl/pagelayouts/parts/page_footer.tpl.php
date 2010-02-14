23714539c49721d73186379e2d7f02d342fd96a9
Remigijus Kiminas <remdex@gmail.com>
2010-02-14 17:07:12 +0000

    Footer with new homepage

--- design/defaulttheme/tpl/pagelayouts/parts/page_footer.tpl.php
+++ design/defaulttheme/tpl/pagelayouts/parts/page_footer.tpl.php
@@ -1,4 +1,4 @@
 <div id="footer">
-    <div class="right"><acronym title="It's NOT fake!">Rendered in: <?=number_format(set_time($GLOBALS['star_microtile'], microtime()), 5);?> s.</acronym>, powered by <a href="http://fof.remdex.info">http://fof.remdex.info</a></div>
+    <div class="right"><acronym title="It's NOT fake!">Rendered in: <?=number_format(set_time($GLOBALS['star_microtile'], microtime()), 5);?> s.</acronym>, powered by <a href="http://redmine.remdex.info/projects/hppg">High performance photo gallery</a></div>
 	<div class="creator copyright">&copy; <?=date('Y')?></div>
 </div>
