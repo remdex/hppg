f4f642588b595694d63c6f355188c69f719c82bb
Remigijus Kiminas <remdex@gmail.com>
2010-02-14 16:12:16 +0000

    Cache configs

---
+++ cache/cacheconfig/settings.ini.php
@@ -0,0 +1,13 @@
+<?php
+return array (
+  'settings' => 
+  array (
+    'cachetimestamps' => 
+    array (
+      'translationfile' => 0,
+      'accessfile' => 1253805894,
+    ),
+  ),
+  'comments' => NULL,
+);
+?>
