75e27f08cebace63aa302efa7704d04346d34c09
Remigijus Kiminas <remdex@gmail.com>
2010-02-16 08:40:45 +0000

    Last hits rss

---
+++ modules/lhgallery/lasthitsrss.php
@@ -0,0 +1,42 @@
+<?php
+
+header ("content-type: text/xml");
+$cache = CSCacheAPC::getMem(); 
+$cacheVersion = $cache->getCacheVersion('last_hits_version',time(),600);
+
+if (($xml = $cache->restore(md5($cacheVersion.'_rss_last_hits'))) === false)
+{         
+    $feed = new ezcFeed(); 
+    $feed->title = 'Last viewed images';
+    $feed->description = '';
+    $feed->published = time(); 
+    $link = $feed->add( 'link' );
+    $link->href = 'http://'.$_SERVER['HTTP_HOST'].erLhcoreClassDesign::baseurl('/gallery/lasthitsrss/');     
+    $items = erLhcoreClassModelGalleryImage::getImages(array('smart_select' => true,'disable_sql_cache' => true,'sort' => 'mtime DESC, pid DESC','offset' => 0, 'limit' => 20));    
+    foreach ($items as $itemRecord)
+    {	
+    	
+    	    $item = $feed->add( 'item' ); 
+    	    $item->title = ($title = $itemRecord->name_user) == '' ? 'View image' : $title;
+    	    $item->description = htmlspecialchars('<img src="'.erLhcoreClassDesign::imagePath($itemRecord->filepath.'thumb_'.urlencode($itemRecord->filename)).'" alt="'.htmlspecialchars($itemRecord->name_user).'" />').	    
+    	   '<ul>
+                <li>'.$itemRecord->pwidth.'x'.$itemRecord->pheight.'</li>
+                <li>'.$itemRecord->hits.' watched</li>                    
+                </a></li>
+            </ul>';;
+    	    $item->published = $itemRecord->ctime; 
+    	     
+    	    $link = $item->add( 'link' );
+    	    $link->href = 'http://'.$_SERVER['HTTP_HOST'].$itemRecord->url_path.'/(mode)/lasthits'; 
+    	
+    }
+    
+    $xml = $feed->generate( 'rss2' ); 
+    
+    $cache->store(md5($cacheVersion.'_rss_last_hits'),$xml);
+}
+
+echo $xml;
+exit;
+
+?>
