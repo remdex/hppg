e4d9868d138d34b9aa473b2ed777a78f870912a8
Remigijus Kiminas <remdex@gmail.com>
2010-02-18 20:08:17 +0000

    Password forget functionality translations

--- design/defaulttheme/tpl/lhuser/forgotpasswordsent.tpl.php
+++ design/defaulttheme/tpl/lhuser/forgotpasswordsent.tpl.php
@@ -1,6 +1,6 @@
 <div class="header-list">
-<h1>Password remind</h1>
+<h1><?=erTranslationClassLhTranslation::getInstance()->getTranslation('user/forgotpasswordsent','Password remind');?></h1>
 </div>
 <div class="attribute-short">
-    <p>Email send. Click in link in email and to you will be send out new password.</p>
+    <p><?=erTranslationClassLhTranslation::getInstance()->getTranslation('user/forgotpasswordsent','E-mail send. Click link in email and to You will be send out new password.')?></p>
 </div> 
