add11cfc6e0a95babae7385a1d3d3b406d04f78d
Remigijus Kiminas <remdex@gmail.com>
2010-02-18 20:13:23 +0000

    Password remind second step

--- modules/lhuser/remindpassword.php
+++ modules/lhuser/remindpassword.php
@@ -2,7 +2,7 @@
 
 $tpl = erLhcoreClassTemplate::getInstance( 'lhuser/remindpassword.tpl.php');
 
-$msg = 'Hash not found or was used already';
+$msg = erTranslationClassLhTranslation::getInstance()->getTranslation('user/remindpassword','Hash not found or was used already');
 
 $hash = $Params['user_parameters']['hash'];
 
@@ -26,14 +26,14 @@ if ($hash != '') {
 			$mail = new PHPMailer();
 			$mail->CharSet = "UTF-8";
 			$mail->From = $adminEmail;
-			$mail->FromName = 'hentaiwallpapers.com - hentai wallpapers';
-			$mail->Subject = "Password remind - new passowrd";
+			$mail->FromName = erConfigClassLhConfig::getInstance()->conf->getSetting( 'site', 'title' );
+			$mail->Subject = erTranslationClassLhTranslation::getInstance()->getTranslation('user/remindpassword','Password remind - new password');
 	
 			// HTML body
-			$body  = 'New password: '.$password;
+			$body  = erTranslationClassLhTranslation::getInstance()->getTranslation('user/remindpassword','New password:').' '.$password;
 	
 			// Plain text body
-			$text_body  = 'New password: '.$password;		
+			$text_body  = erTranslationClassLhTranslation::getInstance()->getTranslation('user/remindpassword','New password:').' '.$password;		
 	
 			$mail->Body    = $body;
 			$mail->AltBody = $text_body;
@@ -42,7 +42,7 @@ if ($hash != '') {
 			$mail->Send();			
 			$mail->ClearAddresses();
 			
-			$msg = 'New password has beend send to your email.';
+			$msg = erTranslationClassLhTranslation::getInstance()->getTranslation('user/remindpassword','New password has beend send to your email.');
 			
 			erLhcoreClassModelForgotPassword::deleteHash($hashData['id']);
 			
