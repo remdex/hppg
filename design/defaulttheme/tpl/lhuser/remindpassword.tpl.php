bee226bcb720c5b35c2cec894862a3fef9a5a604
Remigijus Kiminas <remdex@gmail.com>
2010-02-18 20:14:35 +0000

    Password remind second step

--- design/defaulttheme/tpl/lhuser/remindpassword.tpl.php
+++ design/defaulttheme/tpl/lhuser/remindpassword.tpl.php
@@ -1,5 +1,5 @@
 <div class="header-list">
-<h1>New password<h1>
+<h1><?=erTranslationClassLhTranslation::getInstance()->getTranslation('user/remindpassword','New password')?><h1>
 </div>
 
 <div class="attribute-short"><p><?=$msg?></p></div>
