5bbc52045bceb82c9a7b3b0cf73573b0eace223e
Remigijus Kiminas <remdex@gmail.com>
2010-05-07 18:56:55 +0000

    Unneded pagelayout setters

--- modules/lhpermission/module.php
+++ modules/lhpermission/module.php
@@ -8,49 +8,42 @@ $ViewList['roles'] = array(
     'script' => 'roles.php',
     'params' => array(),
     'functions' => array( 'list' ),
-    'pagelayout' => 'admin'
     );    
     
 $ViewList['newrole'] = array( 
     'script' => 'newrole.php',
     'params' => array(),
     'functions' => array( 'new' ),
-    'pagelayout' => 'admin'
     );    
      
 $ViewList['editrole'] = array( 
     'script' => 'editrole.php',
     'params' => array('role_id'),
     'functions' => array( 'edit' ),
-    'pagelayout' => 'admin'
     );    
       
 $ViewList['modulefunctions'] = array( 
     'script' => 'modulefunctions.php',
     'params' => array('module_path'),
     'functions' => array( 'edit' ),
-    'pagelayout' => 'admin'
 );   
     
 $ViewList['deleterole'] = array( 
     'script' => 'deleterole.php',
     'params' => array('role_id'),
     'functions' => array( 'delete' ),
-    'pagelayout' => 'admin'
 );   
   
 $ViewList['groupassignrole'] = array( 
     'script' => 'groupassignrole.php',
     'params' => array('group_id'),
     'functions' => array( 'edit' ),
-    'pagelayout' => 'admin'
 );   
 
 $ViewList['roleassigngroup'] = array( 
     'script' => 'roleassigngroup.php',
     'params' => array('role_id'),
     'functions' => array( 'edit' ),
-    'pagelayout' => 'admin'
 ); 
       
 $FunctionList['edit'] = array('explain' => 'Access to edit role');  
