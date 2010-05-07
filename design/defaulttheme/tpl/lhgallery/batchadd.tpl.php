936c1eaedf04154f0c0ed3737a6a057bfae27e7a
Remigijus Kiminas <remdex@gmail.com>
2010-05-07 15:44:28 +0000

    Bugfix 9

--- design/defaulttheme/tpl/lhgallery/batchadd.tpl.php
+++ design/defaulttheme/tpl/lhgallery/batchadd.tpl.php
@@ -2,7 +2,7 @@
 <ul>
 <?php 
 foreach ($directoryList  as $directory) : ?>
-<li><a href="<?=erLhcoreClassDesign::baseurl('/gallery/batchadd/(directory)/')?><?=urlencode($directory);?>"><?=$directory?></a> | <a href="<?=erLhcoreClassDesign::baseurl('/gallery/batchadd/(directory)/')?><?=urlencode($directory);?>/(import)/1"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('gallery/batchadd','Import')?></a> | <a href="<?=erLhcoreClassDesign::baseurl('/gallery/batchadd/(directory)/')?><?=urlencode($directory);?>/(importrecur)/1"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('gallery/batchadd','Import recursive this directory')?></a></li>
+<li><a href="<?=erLhcoreClassDesign::baseurl('/gallery/batchadd')?>/(directory)/<?=urlencode($directory);?>"><?=$directory?></a> | <a href="<?=erLhcoreClassDesign::baseurl('/gallery/batchadd')?>/(directory)/<?=urlencode($directory);?>/(import)/1"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('gallery/batchadd','Import')?></a> | <a href="<?=erLhcoreClassDesign::baseurl('/gallery/batchadd')?>/(directory)/<?=urlencode($directory);?>/(importrecur)/1"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('gallery/batchadd','Import recursive this directory')?></a></li>
 <?endforeach;?>
 </ul>
 <br />
