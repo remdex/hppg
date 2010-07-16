2cfe8e427e96a39940f60450432ffe86b84ceb5b
Remigijus Kiminas <remdex@gmail.com>
2010-07-16 15:53:21 +0000

    Same mode for favourite images

--- design/defaulttheme/tpl/lhgallery/image_list_favorites.tpl.php
+++ design/defaulttheme/tpl/lhgallery/image_list_favorites.tpl.php
@@ -1,20 +1,24 @@
 <div class="float-break">
 <? foreach ($items as $key => $itemFavorite) : 
-
-$item = $itemFavorite->image;
+try{
+	$item = $itemFavorite->image;
+	$normalPath = ($item->pwidth < 450) ? erLhcoreClassDesign::imagePath($item->filepath.urlencode($item->filename)) : erLhcoreClassDesign::imagePath($item->filepath.'normal_'.urlencode($item->filename));
+} catch (Exception $e){
+	continue;
+}
 ?>
     <div class="image-thumb" id="image_thumb_<?=$item->pid?>">
         <div class="right">
         <a class="cursor" onclick="return hw.deleteFavorite(<?=$item->pid?>)" ><img src="<?=erLhcoreClassDesign::design('images/icons/delete.png');?>" alt="<?=erTranslationClassLhTranslation::getInstance()->getTranslation('gallery/myfavorites','Remove from favorites');?>" title="<?=erTranslationClassLhTranslation::getInstance()->getTranslation('gallery/myfavorites','Remove from favorites');?>" /></a>
         </div>
         <div class="thumb-pic">
-            <a href="<?=$item->url_path?><?=isset($appendImageMode) ? $appendImageMode : ''?>"><img title="See full size" src="<?=erLhcoreClassDesign::imagePath($item->filepath.'thumb_'.urlencode($item->filename))?>" alt="<?=htmlspecialchars($item->name_user);?>" /></a>
+            <a rel="<?=$normalPath?>" id="pic_attr_<?=$item->pid?>" href="<?=$item->url_path?><?=isset($appendImageMode) ? $appendImageMode : ''?>"><img title="See full size" src="<?=erLhcoreClassDesign::imagePath($item->filepath.'thumb_'.urlencode($item->filename))?>" alt="<?=htmlspecialchars($item->name_user);?>" /></a>
         </div>
         <div class="thumb-attr">  
            <ul>
 	            <li><?=$item->pwidth?>x<?=$item->pheight?></li>
 	            <li><?=$item->hits?> watched</li>
-	            <li><h3><a class="thmb" rel="<?=($item->pwidth < 450) ? erLhcoreClassDesign::imagePath($item->filepath.urlencode($item->filename)) : erLhcoreClassDesign::imagePath($item->filepath.'normal_'.urlencode($item->filename))?>" title="<?=htmlspecialchars($item->name_user);?>" href="<?=erLhcoreClassDesign::imagePath($item->filepath.$item->filename)?>">
+	            <li><h3><a class="thmb" id="attr_<?=$item->pid?>" rel="<?=$normalPath?>" title="<?=htmlspecialchars($item->name_user);?>" href="<?=erLhcoreClassDesign::imagePath($item->filepath.$item->filename)?>">
 	            <?=($title = $item->name_user) == '' ? erTranslationClassLhTranslation::getInstance()->getTranslation('gallery/image_list','preview version') : $title;?>          
 	            </a></h3></li>
 	        </ul>            
@@ -30,8 +34,5 @@ $item = $itemFavorite->image;
 </div>
 
 <script type="text/javascript">
-$('.thumb-attr a').each(function(index) {	
-	$(this).colorbox({href:$(this).attr('rel')});	
-	$(this).attr('href','');
-})
+hw.imagePreview();
 </script>
