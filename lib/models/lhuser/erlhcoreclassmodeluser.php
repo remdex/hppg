d7c8f6575a42e77f937f48038dcaaaf36f4608a3
Remigijus Kiminas <remdex@gmail.com>
2010-07-17 15:32:23 +0000

    User list pagination templates

--- lib/models/lhuser/erlhcoreclassmodeluser.php
+++ lib/models/lhuser/erlhcoreclassmodeluser.php
@@ -32,12 +32,146 @@ class erLhcoreClassModelUser {
    	 return $user;
    }
    
+   public static function getUserCount($params = array())
+   {
+       $session = erLhcoreClassUser::getSession();
+       $q = $session->database->createSelectQuery();  
+       $q->select( "COUNT(id)" )->from( "lh_users" );   
+         
+       if (isset($params['filter']) && count($params['filter']) > 0)
+       {
+           $conditions = array();
+           
+           foreach ($params['filter'] as $field => $fieldValue)
+           {
+               $conditions[] = $q->expr->eq( $field, $fieldValue );
+           }
+           
+           $q->where( 
+                 $conditions   
+           );
+      }  
+             
+      $stmt = $q->prepare();       
+      $stmt->execute();  
+      $result = $stmt->fetchColumn(); 
+            
+      return $result; 
+   }
+   
    public function setPassword($password)
    {
        $cfgSite = erConfigClassLhConfig::getInstance();
 	   $secretHash = $cfgSite->conf->getSetting( 'site', 'secrethash' );
        $this->password = sha1($password.$secretHash.sha1($password));
    } 
+   
+   public static function getUserList($paramsSearch = array())
+   {             
+       $paramsDefault = array('limit' => 32, 'offset' => 0);
+       
+       $params = array_merge($paramsDefault,$paramsSearch);
+       
+       $session = erLhcoreClassUser::getSession();
+       $q = $session->createFindQuery( 'erLhcoreClassModelUser' );  
+       
+       $conditions = array(); 
+       if (!isset($paramsSearch['smart_select'])) {
+             
+                  if (isset($params['filter']) && count($params['filter']) > 0)
+                  {                     
+                       foreach ($params['filter'] as $field => $fieldValue)
+                       {
+                           $conditions[] = $q->expr->eq( $field, $fieldValue );
+                       }
+                  } 
+                  
+                  if (isset($params['filterin']) && count($params['filterin']) > 0)
+                  {
+                       foreach ($params['filterin'] as $field => $fieldValue)
+                       {
+                           $conditions[] = $q->expr->in( $field, $fieldValue );
+                       } 
+                  }
+                  
+                  if (isset($params['filterlt']) && count($params['filterlt']) > 0)
+                  {
+                       foreach ($params['filterlt'] as $field => $fieldValue)
+                       {
+                           $conditions[] = $q->expr->lt( $field, $fieldValue );
+                       } 
+                  }
+                  
+                  if (isset($params['filtergt']) && count($params['filtergt']) > 0)
+                  {
+                       foreach ($params['filtergt'] as $field => $fieldValue)
+                       {
+                           $conditions[] = $q->expr->gt( $field, $fieldValue );
+                       } 
+                  }      
+                  
+                  if (count($conditions) > 0)
+                  {
+                      $q->where( 
+                                 $conditions   
+                      );
+                  } 
+                  
+                  $q->limit($params['limit'],$params['offset']);
+                            
+                  $q->orderBy(isset($params['sort']) ? $params['sort'] : 'id DESC' ); 
+       } else {
+           $q2 = $q->subSelect();
+           $q2->select( 'pid' )->from( 'lh_users' );
+           
+           if (isset($params['filter']) && count($params['filter']) > 0)
+          {                     
+               foreach ($params['filter'] as $field => $fieldValue)
+               {
+                   $conditions[] = $q2->expr->eq( $field, $fieldValue );
+               }
+          } 
+          
+          if (isset($params['filterin']) && count($params['filterin']) > 0)
+          {
+               foreach ($params['filterin'] as $field => $fieldValue)
+               {
+                   $conditions[] = $q2->expr->in( $field, $fieldValue );
+               } 
+          }
+          
+          if (isset($params['filterlt']) && count($params['filterlt']) > 0)
+          {
+               foreach ($params['filterlt'] as $field => $fieldValue)
+               {
+                   $conditions[] = $q2->expr->lt( $field, $fieldValue );
+               } 
+          }
+          
+          if (isset($params['filtergt']) && count($params['filtergt']) > 0)
+          {
+               foreach ($params['filtergt'] as $field => $fieldValue)
+               {
+                   $conditions[] = $q2->expr->gt( $field, $fieldValue );
+               } 
+          }      
+          
+          if (count($conditions) > 0)
+          {
+              $q2->where( 
+                         $conditions   
+              );
+          }
+           
+          $q2->limit($params['limit'],$params['offset']);
+          $q2->orderBy(isset($params['sort']) ? $params['sort'] : 'id DESC');
+          $q->innerJoin( $q->alias( $q2, 'items' ), 'lh_users.id', 'items.id' );          
+       }
+              
+       $objects = $session->find( $q, 'erLhcoreClassModelUser' );
+         
+      return $objects; 
+   }
    
    public static function userExists($username)
    {
