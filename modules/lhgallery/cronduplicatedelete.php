09e1366401ed34cc34c8d87248d93c193afa2f3d
Remigijus Kiminas <remdex@gmail.com>
2010-07-22 09:54:53 +0000

    Cronjob script withc deletes pending duplicates, older images are kept.

---
+++ modules/lhgallery/cronduplicatedelete.php
@@ -0,0 +1,42 @@
+<?php
+
+$session = erLhcoreClassGallery::getSession();
+
+$q = $session->createFindQuery( 'erLhcoreClassModelGalleryDuplicateCollection' );
+$objects = $session->find( $q, 'erLhcoreClassModelGalleryDuplicateCollection' ); 
+
+foreach ($objects as $object)
+{	
+	$originalImageId = null;
+	$oldestID = null;
+	
+	foreach ($object->duplicate_images as $imageDuplicateItem) { 
+    	 $imageDuplicate = $imageDuplicateItem->image;
+    	 
+    	 if ($oldestID == null || $oldestID > $imageDuplicate->pid)
+    	 {
+    	 	$oldestID = $imageDuplicate->pid;
+    	 }    	     	 
+	}
+	
+	echo "Original ID-",$oldestID,"\n";
+	
+	foreach ($object->duplicate_images as $imageDuplicateItem) { 
+    	 $imageDuplicate = $imageDuplicateItem->image;
+    	 
+    	 
+    	 if ($imageDuplicate->pid > 0 && $oldestID != $imageDuplicate->pid)
+    	 {
+    	 	echo "Removing image with ID - ",$imageDuplicate->pid,"\n";
+    	 	$imageDuplicate->removeThis();
+    	 } else {
+    	 	echo "Skipping original - ",$imageDuplicate->pid,"\n";
+    	 }   	     	 
+	}
+	
+	$object->removeThis();
+}
+
+
+
+
