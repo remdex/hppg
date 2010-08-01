3af5f9358c82a8f9a7b760c915c91d507b451587
Remigijus Kiminas <remdex@gmail.com>
2010-08-01 11:04:36 +0000

    Show ordered image name in backed

--- design/backend/tpl/lhshop/orderedit.tpl.php
+++ design/backend/tpl/lhshop/orderedit.tpl.php
@@ -42,6 +42,7 @@
 <h2><?=erTranslationClassLhTranslation::getInstance()->getTranslation('shop/orderedit','Ordered items');?></h2>
 <table class="lentele" cellpadding="0" cellspacing="0">
 <tr>
+    <th><?=erTranslationClassLhTranslation::getInstance()->getTranslation('shop/orderview','Image name')?></th>
 	<th><?=erTranslationClassLhTranslation::getInstance()->getTranslation('shop/orderedit','Version');?></th>
 	<th><?=erTranslationClassLhTranslation::getInstance()->getTranslation('shop/orderedit','Image size');?></th>
 	<th><?=erTranslationClassLhTranslation::getInstance()->getTranslation('shop/orderedit','Format');?></th>
@@ -52,6 +53,7 @@
 </tr>
 <?php foreach ($order->order_items as $item) : ?>
 	<tr>
+	    <td><a href="<?=$item->image->url_path?>"><?=htmlspecialchars($item->image->name_user)?></a></td>
 		<td><?=$item->image_variation->name?></td>
 		<td><?=$item->image_variation->getSize($item->image)?></td>
 		<td><?=$item->image_variation->getFormat($item->image)?></td>
