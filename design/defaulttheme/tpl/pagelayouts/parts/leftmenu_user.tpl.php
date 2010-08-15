e96e2bc6ea9d3844fc713667afe30e5170dfbf43
Remigijus Kiminas <remdex@gmail.com>
2010-08-15 15:24:15 +0000

    Left menu missing permission checking

--- design/defaulttheme/tpl/pagelayouts/parts/leftmenu_user.tpl.php
+++ design/defaulttheme/tpl/pagelayouts/parts/leftmenu_user.tpl.php
@@ -3,9 +3,12 @@
     <li><a href="<?=erLhcoreClassDesign::baseurl('user/account')?>"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','My account');?></a></li>
 	<?php if (erLhcoreClassUser::instance()->hasAccessTo('lhgallery','personal_albums')) : ?>
 	<li><a href="<?=erLhcoreClassDesign::baseurl('gallery/myalbums')?>"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Manage albums');?></a></li>
-	<?php endif;?>		
+	<?php endif;?>
+	
+	<?php if (erLhcoreClassUser::instance()->hasAccessTo('lhshop','use')) : ?>		
 	<li><a href="<?=erLhcoreClassDesign::baseurl('shop/mycredits')?>"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Credits');?></a></li>
 	<li><a href="<?=erLhcoreClassDesign::baseurl('shop/myorders')?>"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','My orders');?></a></li>
 	<li><a href="<?=erLhcoreClassDesign::baseurl('shop/mycreditsorders')?>"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','My credits orders');?></a></li>
+	<?php endif;?>
 </ul>
 	
