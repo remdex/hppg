060fad9eac9f0f733b71672c381303fc20557044
Remigijus Kiminas <remdex@gmail.com>
2010-08-15 12:41:38 +0000

    No path in homepage

--- design/defaulttheme/tpl/pagelayouts/parts/path.tpl.php
+++ design/defaulttheme/tpl/pagelayouts/parts/path.tpl.php
@@ -1,4 +1,6 @@
 <? if (isset($Result['path'])) : 		
 $pathElementCount = count($Result['path'])-1;
+if ($pathElementCount >= 0):
 ?>			
 <div id="path"><a href="/"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Home')?></a><? foreach ($Result['path'] as $key => $pathItem) : if (isset($pathItem['url'])) { ?><a href="<?=$pathItem['url']?>"><?=$pathItem['title']?></a><? } else { ?><?=$pathItem['title']?><? }; ?><? endforeach; ?></div><? endif; ?>
+<?php endif;?>
