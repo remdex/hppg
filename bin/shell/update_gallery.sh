86a8c717a90af58cd59a8f434ed9b930d80893b4
Remigijus Kiminas <remdex@gmail.com>
2010-08-18 17:32:18 +0000

    Gallery update script, update from SVN

---
+++ bin/shell/update_gallery.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+if [ ! -f "cron.php" -a \
+     ! -f "index.php" -a \
+     ! -f "runtest.php" -a \
+     ! -d "design" -a \
+     ! -d "lib" -a \
+     ! -d "pos" -a \
+     ! -d "modules" ] ; then
+     echo "You seem to be in the wrong directory"
+     echo "Place yourself in the HPPG root directory and run ./bin/shell/update_gallery.sh"
+     exit 1
+fi
+
+echo "Updating design directory using SVN"
+svn update "./design"
+
+echo "Updating doc files"
+svn update "./doc"
+
+echo "Updating eZComponents"
+svn update "./ezcomponents"
+
+echo "Updating library files"
+svn update "./lib"
+
+echo "Updating modules"
+svn update "./modules"
+
+echo "Updating pos files"
+svn update "./pos"
+
+echo "Updating translations"
+svn update "./translations"
+
+echo "Updating Zend Framework"
+svn update "./Zend"
