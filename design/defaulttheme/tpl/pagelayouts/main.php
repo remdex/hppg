1ac4f209ed85f58de54e887137c2c6b4e151cd9b
Remigijus Kiminas <remdex@gmail.com>
2010-08-20 15:14:17 +0000

    Separated last hits infobox and increased image fetch to 4

--- design/defaulttheme/tpl/pagelayouts/main.php
+++ design/defaulttheme/tpl/pagelayouts/main.php
@@ -58,33 +58,10 @@
 					</ul>									
              </div>
            
-             <?php include_once(erLhcoreClassDesign::designtpl('pagelayouts/parts/leftmenu_last_searchers.tpl.php'));?>
+            <?php include_once(erLhcoreClassDesign::designtpl('pagelayouts/parts/leftmenu_last_searchers.tpl.php'));?>
                          
-            	 
-             <div class="left-infobox">                    
-                <h3><a href="<?=erLhcoreClassDesign::baseurl('gallery/lasthits')?>"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Last viewed images')?></a></h3>
-                <?php 
-                $cache = CSCacheAPC::getMem(); 
-                $cacheVersion = $cache->getCacheVersion('last_hits_version',time(),600);
-                if (($ResultCache = $cache->restore(md5($cacheVersion.'_lasthits_infobox'))) === false)
-                {
-                    $items = erLhcoreClassModelGalleryImage::getImages(array('disable_sql_cache' => true,'sort' => 'mtime DESC, pid DESC','offset' => 0, 'limit' => 2));
-                    $appendImageMode = '/(mode)/lasthits';
-                    $ResultCache = '<ul class="last-hits-infobox">';                                                        
-                    foreach ($items as $item)
-                    {      
-                       $ResultCache .= '<li><a href="'.$item->url_path.$appendImageMode.'"><img title="'.erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','View image').'" src="'.erLhcoreClassDesign::imagePath($item->filepath.'thumb_'.urlencode($item->filename),true,$item->pid).'" alt="'.htmlspecialchars($item->name_user).'" /></a></li>';
-                    }                            
-                    $ResultCache .= '</ul>';
-                    
-                    $cache->store(md5($cacheVersion.'_lasthits_infobox'),$ResultCache);
-                  
-                }
-                echo $ResultCache;
-                ?>                                         							
-            </div>
-                  
-                 	      
+            <?php include_once(erLhcoreClassDesign::designtpl('pagelayouts/parts/leftmenu_last_hits.tpl.php'));?>	 
+               	      
         </div>
         
         	
---
+++ design/defaulttheme/tpl/pagelayouts/parts/leftmenu_last_hits.tpl.php
@@ -0,0 +1,22 @@
+<div class="left-infobox">                    
+    <h3><a href="<?=erLhcoreClassDesign::baseurl('gallery/lasthits')?>"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Last viewed images')?></a></h3>
+    <?php 
+    $cache = CSCacheAPC::getMem(); 
+    $cacheVersion = $cache->getCacheVersion('last_hits_version',time(),600);
+    if (($ResultCache = $cache->restore(md5($cacheVersion.'_lasthits_infobox'))) === false)
+    {
+        $items = erLhcoreClassModelGalleryImage::getImages(array('disable_sql_cache' => true,'sort' => 'mtime DESC, pid DESC','offset' => 0, 'limit' => 4));
+        $appendImageMode = '/(mode)/lasthits';
+        $ResultCache = '<ul class="last-hits-infobox">';                                                        
+        foreach ($items as $item)
+        {      
+           $ResultCache .= '<li><a href="'.$item->url_path.$appendImageMode.'"><img title="'.erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','View image').'" src="'.erLhcoreClassDesign::imagePath($item->filepath.'thumb_'.urlencode($item->filename),true,$item->pid).'" alt="'.htmlspecialchars($item->name_user).'" /></a></li>';
+        }                            
+        $ResultCache .= '</ul>';
+        
+        $cache->store(md5($cacheVersion.'_lasthits_infobox'),$ResultCache);
+      
+    }
+    echo $ResultCache;
+    ?>                                         							
+</div>
