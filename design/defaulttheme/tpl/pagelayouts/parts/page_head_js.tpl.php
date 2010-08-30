2f28d0b6ca3756e24ec409d6fcdfaa17e4a66366
Remigijus Kiminas <remdex@gmail.com>
2010-08-30 16:42:03 +0000

    No need jQuery UI in user interface

--- design/defaulttheme/tpl/pagelayouts/parts/page_head_js.tpl.php
+++ design/defaulttheme/tpl/pagelayouts/parts/page_head_js.tpl.php
@@ -2,6 +2,5 @@
 WWW_DIR_JAVASCRIPT = '<?=erLhcoreClassDesign::baseurl()?>';
 </script>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>
 <script type="text/javascript" src="<?=erConfigClassLhConfig::getInstance()->conf->getSetting( 'cdn', 'css' )?><?=erLhcoreClassDesign::design('js/hw.js');?>?v=1"></script>
 <?=isset($Result['additional_js']) ? $Result['additional_js'] : ''?>
