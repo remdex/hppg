04d69dcfd840f97122b342fd6415949e3adc9a65
Remigijus Kiminas <remdex@gmail.com>
2010-10-02 05:52:43 +0000

    Clear cache box in left

---
+++ design/backend/tpl/pagelayouts/parts/clear_cache_box.tpl.php
@@ -0,0 +1,5 @@
+<fieldset><legend><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Clear cache')?></legend> 
+<ul>
+   <li><a href="<?=erLhcoreClassDesign::baseurl('system/expirecache')?>">&raquo; <?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Clean all cache');?></a></li>   
+</ul>
+</fieldset>
