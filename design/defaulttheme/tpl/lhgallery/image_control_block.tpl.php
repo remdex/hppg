304c90b1fb8ee7f10f093e95a447010c71f97838
Remigijus Kiminas <remdex@gmail.com>
2010-10-21 18:00:37 +0000

    Allow taging for all users

--- design/defaulttheme/tpl/lhgallery/image_control_block.tpl.php
+++ design/defaulttheme/tpl/lhgallery/image_control_block.tpl.php
@@ -12,11 +12,11 @@
 </div>
 
 <div class="right">
-<?if ($image->owner_id == 1 || $image->owner_id == 0) : ?>
+
 <div class="act-blc" id="tags-container">
 <input type="text" id="IDtagsPhoto" value="" class="inputfield" title="<?=erTranslationClassLhTranslation::getInstance()->getTranslation('gallery/image','Enter tags separated by space')?>" /> <input type="button" onclick="hw.tagphoto(<?=$image->pid;?>)" class="default-button" value="tag it!" /> <i>Tag this photo</i>
 </div>
-<?endif;?>
+
 </div>
 
 </div>
