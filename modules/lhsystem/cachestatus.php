0deaca3e290c2e5713b3abedfefaf8676e94569e
Remigijus Kiminas <remdex@gmail.com>
2010-10-25 16:01:07 +0000

    More features for cache status window

--- modules/lhsystem/cachestatus.php
+++ modules/lhsystem/cachestatus.php
@@ -10,6 +10,7 @@ $tpl->set('top_rated',$cache->getCacheVersion('top_rated'));
 $tpl->set('last_commented',$cache->getCacheVersion('last_commented'));
 $tpl->set('most_popular_version',$cache->getCacheVersion('most_popular_version',time(),1500));
 $tpl->set('popularrecent_version',$cache->getCacheVersion('popularrecent_version',time(),600));
+$tpl->set('site_version',$cache->getCacheVersion('site_version'));
 
 
 $Result['content'] = $tpl->fetch();
--- modules/lhsystem/module.php
+++ modules/lhsystem/module.php
@@ -26,6 +26,18 @@ $ViewList['expirecache'] = array(
     'script' => 'expirecache.php',
     'params' => array(),
     'functions' => array( 'expirecache' )
+    );  
+           
+$ViewList['albumcacheinfo'] = array( 
+    'script' => 'albumcacheinfo.php',
+    'params' => array('album_id'),
+    'functions' => array( 'use' )
+    ); 
+              
+$ViewList['categorycacheinfo'] = array( 
+    'script' => 'categorycacheinfo.php',
+    'params' => array('category_id'),
+    'functions' => array( 'use' )
     ); 
     
 $FunctionList['use'] = array('explain' => 'Allow user to see configuration links');  
