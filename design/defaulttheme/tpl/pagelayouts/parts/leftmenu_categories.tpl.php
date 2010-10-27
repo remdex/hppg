d488b4aea4d7529bbd7c61a463480afba4aa2caf
Remigijus Kiminas <remdex@gmail.com>
2010-10-27 19:50:59 +0000

    Menu translation renaming

--- design/defaulttheme/tpl/pagelayouts/parts/leftmenu_categories.tpl.php
+++ design/defaulttheme/tpl/pagelayouts/parts/leftmenu_categories.tpl.php
@@ -1,7 +1,7 @@
 <div class="left-infobox">				
 		<h3><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','All time')?></h3>
 		<ul>													
-		    <li><a href="<?=erLhcoreClassDesign::baseurl('/gallery/popular')?>"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Most popular images');?></a>                 
+		    <li><a href="<?=erLhcoreClassDesign::baseurl('/gallery/popular')?>"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Most popular');?></a>                 
             <li><a href="<?=erLhcoreClassDesign::baseurl('/gallery/toprated')?>"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Top rated');?></a>                  
 		</ul>									
 </div>
@@ -19,7 +19,7 @@
 <div class="left-infobox">				
 		<h3><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Last 24 h.')?></h3>
 		<ul>  
-            <li><a href="<?=erLhcoreClassDesign::baseurl('/gallery/popularrecent')?>"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Most popular images');?></a>                 
+            <li><a href="<?=erLhcoreClassDesign::baseurl('/gallery/popularrecent')?>"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Most popular');?></a>                 
             <li><a href="<?=erLhcoreClassDesign::baseurl('/gallery/ratedrecent')?>"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Top rated');?></a>                 
 		</ul>									
 </div>
