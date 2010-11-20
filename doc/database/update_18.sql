34e4b552c0e18435ddca8c2bbc7415fa05b858b1
Remigijus Kiminas <remdex@gmail.com>
2010-11-20 12:23:55 +0000

    Ignore primary key

--- doc/database/update_18.sql
+++ doc/database/update_18.sql
@@ -2,4 +2,4 @@ ALTER TABLE  `lh_gallery_searchhistory` DROP  `id`;
 ALTER TABLE  `lh_gallery_searchhistory` DROP  `crc32`;
 ALTER TABLE  `lh_gallery_searchhistory` DROP INDEX  `keyword_2`;
 ALTER TABLE  `lh_gallery_searchhistory` DROP INDEX  `keyword`;
-ALTER TABLE  `lh_gallery_searchhistory` ADD PRIMARY KEY (  `keyword` );
+ALTER IGNORE TABLE  `lh_gallery_searchhistory` ADD PRIMARY KEY (  `keyword` );
