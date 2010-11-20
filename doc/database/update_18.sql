947fb8ffed0d3231db645b978b46ff6fa4f1d546
Remigijus Kiminas <remdex@gmail.com>
2010-11-20 13:19:59 +0000

    German module translations

--- doc/database/update_18.sql
+++ doc/database/update_18.sql
@@ -3,3 +3,4 @@ ALTER TABLE  `lh_gallery_searchhistory` DROP  `crc32`;
 ALTER TABLE  `lh_gallery_searchhistory` DROP INDEX  `keyword_2`;
 ALTER TABLE  `lh_gallery_searchhistory` DROP INDEX  `keyword`;
 ALTER IGNORE TABLE  `lh_gallery_searchhistory` ADD PRIMARY KEY (  `keyword` );
+DELETE FROM `lh_gallery_searchhistory` WHERE keyword LIKE '%+%';
