a24b2bd1da2c649629649ffba03f3737b6431d44
Remigijus Kiminas <remdex@gmail.com>
2010-11-23 18:49:59 +0000

    Encode file path with base64_encode

--- modules/lhgallery/batchadd.php
+++ modules/lhgallery/batchadd.php
@@ -2,10 +2,10 @@
 
 $tpl = erLhcoreClassTemplate::getInstance( 'lhgallery/batchadd.tpl.php');
 
-$directoryList = erLhcoreClassGalleryBatch::listDirectory(isset($Params['user_parameters_unordered']['directory']) ? urldecode($Params['user_parameters_unordered']['directory']) : 'albums');
+$directoryList = erLhcoreClassGalleryBatch::listDirectory(isset($Params['user_parameters_unordered']['directory']) ? base64_decode(urldecode($Params['user_parameters_unordered']['directory'])) : 'albums');
 $tpl->set('directoryList',$directoryList);
 
-$directory = isset($Params['user_parameters_unordered']['directory']) ? urldecode($Params['user_parameters_unordered']['directory']) : 'albums';
+$directory = isset($Params['user_parameters_unordered']['directory']) ? base64_decode(urldecode($Params['user_parameters_unordered']['directory'])) : 'albums';
 
 
 
@@ -35,12 +35,12 @@ $Result['content'] = $tpl->fetch();
 $pathDirectorys = explode('/',$directory);     
 $previousFolder = '';
 
-$path[] = array('url' => erLhcoreClassDesign::baseurl('/gallery/batchadd').'/(directory)/'.urlencode(rtrim($previousFolder,'/')),'title' => erTranslationClassLhTranslation::getInstance()->getTranslation('gallery/batchadd','Mass image upload')); 
+$path[] = array('url' => erLhcoreClassDesign::baseurl('/gallery/batchadd').'/(directory)/'.urlencode(base64_encode(rtrim($previousFolder,'/'))),'title' => erTranslationClassLhTranslation::getInstance()->getTranslation('gallery/batchadd','Mass image upload')); 
 
 foreach ($pathDirectorys as $pathItem)
 {
    $previousFolder .= $pathItem.'/';
-   $path[] = array('url' => erLhcoreClassDesign::baseurl('/gallery/batchadd').'/(directory)/'.urlencode(rtrim($previousFolder,'/')),'title' => $pathItem); 
+   $path[] = array('url' => erLhcoreClassDesign::baseurl('/gallery/batchadd').'/(directory)/'.urlencode(base64_encode(rtrim($previousFolder,'/'))),'title' => $pathItem); 
 }
  
 $Result['path'] = $path;
