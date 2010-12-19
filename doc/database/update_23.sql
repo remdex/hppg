b16a89365ff030a78fd8247b6b4561f24d0ed3e7
Remigijus Kiminas <remdex@gmail.com>
2010-12-19 09:58:16 +0000

    Delete treshhold not mathinc color indexes

--- doc/database/update_23.sql
+++ doc/database/update_23.sql
@@ -4,3 +4,4 @@ ALTER TABLE  `lh_gallery_pallete` ADD INDEX (  `position` );
 UPDATE lh_gallery_pallete SET position = position - 20 WHERE id >= 12 AND id <= 20;
 UPDATE lh_gallery_pallete SET position = 400 WHERE id = 11;
 ALTER TABLE  `lh_gallery_pallete` CHANGE  `position`  `position` INT( 11 ) NOT NULL DEFAULT  '0';
+DELETE FROM `lh_gallery_pallete_images` WHERE `count` < 25;
