f7bd6c016bf8560c2ac41c171a67c61a92f93ffe
Remigijus Kiminas <remdex@gmail.com>
2010-12-24 09:51:55 +0000

    Unindexed images indexer manual script

---
+++ bin/php/index_colors_unindexed.php
@@ -0,0 +1,84 @@
+<?php
+
+ini_set("max_execution_time", "0");
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+
+require_once "./ezcomponents/Base/src/base.php";
+
+function __autoload( $className )
+{
+        ezcBase::autoload( $className );
+}
+
+ezcBase::addClassRepository( './', './lib/autoloads'); 
+
+
+ezcBaseInit::setCallback(
+ 'ezcInitDatabaseInstance',
+ 'erLhcoreClassLazyDatabaseConfiguration'
+);
+
+
+$input = new ezcConsoleInput();
+
+$helpOption = $input->registerOption(
+    new ezcConsoleOption(
+        's',
+        'siteaccess',
+        ezcConsoleInput::TYPE_STRING 
+    )
+);
+
+$maximumNumberImages = $input->registerOption(
+    new ezcConsoleOption(
+        'n',
+        'number',
+        ezcConsoleInput::TYPE_STRING 
+    )
+);
+
+try
+{
+    $input->process();
+}
+catch ( ezcConsoleOptionException $e )
+{
+    die( $e->getMessage() );
+} 
+
+$siteAccessName = 'site_admin';
+if ( !$helpOption->value === false )
+{
+    $siteAccessName = $helpOption->value;
+} 
+
+$maximumNumberImages = 500;
+if ( !$maximumNumberImages->value === false )
+{
+    $maximumNumberImages = $maximumNumberImages->value;
+} 
+
+try
+{
+    $input->process();
+}
+catch ( ezcConsoleOptionException $e )
+{
+    die( $e->getMessage() );
+}
+
+$instance = erLhcoreClassSystem::instance();
+$instance->SiteAccess = $siteAccessName; 
+$instance->SiteDir = './';
+$cfgSite = erConfigClassLhConfig::getInstance();    
+$defaultSiteAccess = $cfgSite->conf->getSetting( 'site', 'default_site_access' );
+$optionsSiteAccess = $cfgSite->conf->getSetting('site_access_options',$siteAccessName);                      
+$instance->Language = $optionsSiteAccess['locale'];                         
+$instance->ThemeSite = $optionsSiteAccess['theme'];                         
+$instance->WWWDirLang = '/'.$siteAccessName;   
+
+echo "Starting manual unindexed color images indexing\n";
+
+// We limit 
+erLhcoreClassPalleteIndexImage::indexUnindexedImages($maximumNumberImages);
