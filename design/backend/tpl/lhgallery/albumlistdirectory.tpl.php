8ef491a1f9989170bcadc37549025b97f6bbc4fd
Remigijus Kiminas <remdex@gmail.com>
2011-03-09 16:43:58 +0000

    Avoid duplicate batch import on some types of files

--- design/backend/tpl/lhgallery/albumlistdirectory.tpl.php
+++ design/backend/tpl/lhgallery/albumlistdirectory.tpl.php
@@ -7,7 +7,7 @@
 <?php if (count($filesList) > 0) : ?>
 <table>
 	<? foreach ($filesList as $file) : 
-	if (!preg_match('/^(normal_|thumb_)/i',basename($file)) && !file_exists(dirname($file).'/thumb_'.basename($file))) :
+	if (!preg_match('/^(normal_|thumb_)/i',basename($file)) && !file_exists(dirname($file).'/thumb_'.str_replace(array('.swf','.avi','.mpg','.mpeg','.wmv','.ogv','.flv'),'.jpg',basename($file)))) :
 	?>
 	    <tr>
 	        <td><?=$file?><img class="image_import" rel="<?=base64_encode($file);?>" src="<?=erLhcoreClassDesign::design('images/icons/delete.png');?>"/></td>
