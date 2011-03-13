7e8bf36a2470d38ccc9daa72263e1875c8b7e43d
Remigijus Kiminas <remdex@gmail.com>
2011-03-13 14:07:02 +0000

    Merge all script's to single file

--- design/backend/tpl/pagelayouts/parts/page_head.tpl.php
+++ design/backend/tpl/pagelayouts/parts/page_head.tpl.php
@@ -16,8 +16,5 @@ foreach ($ReverseOrder as $pathItem) : ?>
 <script type="text/javascript">
 WWW_DIR_JAVASCRIPT = '<?=erLhcoreClassDesign::baseurl()?>';
 </script>
-<script type="text/javascript" language="javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
-<script type="text/javascript" language="javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>
-<script type="text/javascript" language="javascript" src="<?=erLhcoreClassDesign::design('js/lh.js');?>"></script>
-<script type="text/javascript" language="javascript" src="<?=erLhcoreClassDesign::design('js/hw.js');?>"></script>
+<script type="text/javascript" language="javascript" src="<?=erLhcoreClassDesign::design('js/hw.js');?>?v=25"></script>
 <?=isset($Result['additional_js']) ? $Result['additional_js'] : ''?>
--- design/defaulttheme/tpl/pagelayouts/parts/page_head_js.tpl.php
+++ design/defaulttheme/tpl/pagelayouts/parts/page_head_js.tpl.php
@@ -1,6 +1,5 @@
 <script>
 WWW_DIR_JAVASCRIPT = '<?=erLhcoreClassDesign::baseurl()?>';
 </script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
-<script src="<?=erConfigClassLhConfig::getInstance()->conf->getSetting( 'cdn', 'css' )?><?=erLhcoreClassDesign::design('js/hw.js');?>?v=2"></script>
+<script src="<?=erConfigClassLhConfig::getInstance()->conf->getSetting( 'cdn', 'css' )?><?=erLhcoreClassDesign::design('js/hw.js');?>?v=25"></script>
 <?=isset($Result['additional_js']) ? $Result['additional_js'] : ''?>
