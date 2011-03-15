73bc301f69938733adf688c968f41ad7ca301052
Remigijus Kiminas <remdex@gmail.com>
2011-03-15 18:06:10 +0000

    Email and link icons

--- lib/core/lhgallery/lhgallery.php
+++ lib/core/lhgallery/lhgallery.php
@@ -25,9 +25,9 @@ class erLhcoreClassGallery{
    public static function make_clickable($text)
    {
         $ret = ' ' . $text;
-        $ret = preg_replace("#(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t<]*)#ise", "'\\1<a target=\"_blank\" href=\"\\2\" >\\2</a>'", $ret);
-        $ret = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r<]*)#ise", "'\\1<a target=\"_blank\" href=\"http://\\2\" >\\2</a>'", $ret);
-        $ret = preg_replace("#(^|[\n ])([a-z0-9&\-_\.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>", $ret);
+        $ret = preg_replace("#(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t<]*)#ise", "'\\1<a class=\"link\" target=\"_blank\" href=\"\\2\" >\\2</a>'", $ret);
+        $ret = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r<]*)#ise", "'\\1<a class=\"link\" target=\"_blank\" href=\"http://\\2\" >\\2</a>'", $ret);
+        $ret = preg_replace("#(^|[\n ])([a-z0-9&\-_\.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", "\\1<a class=\"mail\" href=\"mailto:\\2@\\3\">\\2@\\3</a>", $ret);
         $ret = substr($ret, 1);
         return($ret);
    }
