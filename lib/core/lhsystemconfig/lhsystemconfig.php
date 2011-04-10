90d17f0e4df7b302f0dec32a9b7bc92d9cad2ca3
Remigijus Kiminas <remdex@gmail.com>
2011-04-10 17:17:18 +0000

    Config session for slave

--- lib/core/lhsystemconfig/lhsystemconfig.php
+++ lib/core/lhsystemconfig/lhsystemconfig.php
@@ -7,20 +7,29 @@ class erLhcoreClassSystemConfig {
  
    }
    
-   public static function getSession()
-   {
-        if ( !isset( self::$persistentSession ) )
-        {            
+     
+   public static function getSession($type = false)
+   {                
+        if ($type === false && !isset( self::$persistentSession ) )
+        {
             self::$persistentSession = new ezcPersistentSession(
                 ezcDbInstance::get(),
                 new ezcPersistentCodeManager( './pos/lhsystemconfig' )
             );
+        } elseif ($type !== false && !isset( self::$persistentSessionSlave ) ) {            
+            self::$persistentSessionSlave = new ezcPersistentSession(
+                ezcDbInstance::get($type),
+                new ezcPersistentCodeManager( './pos/lhsystemconfig' )
+            );
         }
-        return self::$persistentSession;
+        
+        return $type === false ? self::$persistentSession : self::$persistentSessionSlave;        
    }
-   
         
    private static $persistentSession;
+   
+   // For selects
+   private static $persistentSessionSlave;
 
 }
 
