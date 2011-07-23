39ac519ea429ad89eb8df14db9765cab4b73b64a
Remigijus Kiminas <remdex@gmail.com>
2011-07-23 16:07:22 +0000

    Mistype fix

--- design/defaulttheme/tpl/lhgallery/showalbuminfo.tpl.php
+++ design/defaulttheme/tpl/lhgallery/showalbuminfo.tpl.php
@@ -5,7 +5,7 @@
 
 <?php 
 $appendImageMode = '';
-$items = erLhcoreClassModelGalleryImage::getImages(array('smart_select' => true,'user_index' => 'pid_6', 'disable_sql_cache' => false,'filter' => array('aid' => $album->aid),'offset' => 0, 'limit' => 3)) ?>
+$items = erLhcoreClassModelGalleryImage::getImages(array('smart_select' => true,'use_index' => 'pid_6', 'disable_sql_cache' => false,'filter' => array('aid' => $album->aid),'offset' => 0, 'limit' => 3)) ?>
 
 <?php include_once(erLhcoreClassDesign::designtpl('lhgallery/image_list.tpl.php'));?>   
 
