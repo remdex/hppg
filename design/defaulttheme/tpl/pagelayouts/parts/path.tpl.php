7853f6a9f850888299fd56b6d68144bf8e250b9b
Remigijus Kiminas <remdex@gmail.com>
2011-08-09 06:24:29 +0000

    Escape title

--- design/defaulttheme/tpl/pagelayouts/parts/path.tpl.php
+++ design/defaulttheme/tpl/pagelayouts/parts/path.tpl.php
@@ -2,5 +2,5 @@
 $pathElementCount = count($Result['path'])-1;
 if ($pathElementCount >= 0):
 ?>			
-<div id="path"><a href="<?=erLhcoreClassDesign::baseurl()?>"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Home')?></a><? foreach ($Result['path'] as $key => $pathItem) : if (isset($pathItem['url']) && $pathElementCount != $key) { ?><a href="<?=$pathItem['url']?>"><?=$pathItem['title']?></a><? } else { ?><?=$pathItem['title']?><? }; ?><? endforeach; ?></div><? endif; ?>
+<div id="path"><a href="<?=erLhcoreClassDesign::baseurl()?>"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Home')?></a><? foreach ($Result['path'] as $key => $pathItem) : if (isset($pathItem['url']) && $pathElementCount != $key) { ?><a href="<?=$pathItem['url']?>"><?=htmlspecialchars($pathItem['title'])?></a><? } else { ?><?=htmlspecialchars($pathItem['title'])?><? }; ?><? endforeach; ?></div><? endif; ?>
 <?php endif;?>
