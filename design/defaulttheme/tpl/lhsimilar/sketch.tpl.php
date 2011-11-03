450ca4ce48c38b31a7a24e26ab475cf924525245
Remigijus Kiminas <remdex@gmail.com>
2011-11-03 20:05:39 +0000

    Paint web for sketch tool

--- design/defaulttheme/tpl/lhsimilar/sketch.tpl.php
+++ design/defaulttheme/tpl/lhsimilar/sketch.tpl.php
@@ -1,3 +1,10 @@
+<div class="header-list">
+            <h1>Search by sketch</h1>
+</div>
+    
+<?php 
+
+/*
 <div id="colors_demo">
 <div class="tools">
   <a href="#colors_sketch" data-download="jpg" data-url="/similar/uploadcanvas" style="float: right; width: 100px;">Search</a>
@@ -18,6 +25,77 @@
     $('#colors_sketch').sketch();
   });
 </script>
+*/ ?>
+
+<p><a style="display:none;" title="Freshalicious" 
+      href="http://www.robodesign.ro/marius/my-projects/images/freshalicious"><img 
+      id="editableImage" src="<?=erLhcoreClassDesign::design('js/paintweb/bgedit.jpg')?>" alt="Freshalicious"></a></p>
+
+    <div id="PaintWebTarget"></div>
+
+    <script type="text/javascript" src="<?=erLhcoreClassDesign::design('js/paintweb/build/paintweb.js')?>"></script>
+
+    <script type="text/javascript"><!--
+(function () { 
+  // Function called when the PaintWeb application fires the "appInit" event.
+  function pwInit (ev) {
+    var initTime = (new Date()).getTime() - timeStart,
+        str = 'Demo: Yay, PaintWeb loaded in ' + initTime + ' ms! ' +
+              pw.toString();
+
+    document.body.removeChild(loadp);
+
+    if (ev.state === PaintWeb.INIT_ERROR) {
+      alert('Demo: PaintWeb initialization failed.');
+      return;
+
+    } else if (ev.state === PaintWeb.INIT_DONE) {
+      if (window.console && console.log) {
+        console.log(str);
+      } else if (window.opera) {
+        opera.postError(str);
+      }
+
+    } else {
+      alert('Demo: Unrecognized PaintWeb initialization state ' + ev.state);
+
+      return;
+    }
+    img.style.display = 'none';
+  };
+
+  var img    = document.getElementById('editableImage'),
+      target = document.getElementById('PaintWebTarget'),
+      loadp  = document.createElement('p'),
+      timeStart = null,
+
+      // Create a PaintWeb instance.
+      pw = new PaintWeb();
+
+      pw.config.guiPlaceholder = target;
+      pw.config.imageLoad      = img; 
+      pw.config.uploadCanvasURL      = '<?=erLhcoreClassDesign::baseurl('similar/uploadcanvas')?>';
+      pw.config.configFile     = 'config-example.json';
+      loadp.appendChild(document.createTextNode('Loading, please wait...'));
+    
+      timeStart = (new Date()).getTime();
+      pw.init(pwInit);
+
+})();
+    --></script>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <div id="img-list-search">
 </div>
--- modules/lhsimilar/sketch.php
+++ modules/lhsimilar/sketch.php
@@ -2,5 +2,4 @@
 
 $tpl = erLhcoreClassTemplate::getInstance( 'lhsimilar/sketch.tpl.php');
 $Result['content'] = $tpl->fetch();
-$Result['additional_js'] = '<script type="text/javascript" language="javascript" src="'.erLhcoreClassDesign::design('js/sketch.min.js').'"></script>';
-$path = array(array('title' => 'Sketch and find images by your sketch'));
+$Result['path'] = array(array('title' => 'Sketch and find images by your sketch'));
