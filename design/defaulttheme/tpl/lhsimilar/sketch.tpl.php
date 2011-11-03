75b388d9be5641161dd60d5d8f87a64e6f6b3fdf
Remigijus Kiminas <remdex@gmail.com>
2011-11-03 20:12:31 +0000

    Remove comment tags

--- design/defaulttheme/tpl/lhsimilar/sketch.tpl.php
+++ design/defaulttheme/tpl/lhsimilar/sketch.tpl.php
@@ -2,31 +2,6 @@
             <h1>Search by sketch</h1>
 </div>
     
-<?php 
-
-/*
-<div id="colors_demo">
-<div class="tools">
-  <a href="#colors_sketch" data-download="jpg" data-url="/similar/uploadcanvas" style="float: right; width: 100px;">Search</a>
-  &nbsp;
-  <a href="#colors_sketch" data-resetmap="data-reset" style="float: right; width: 100px;margin-right:5px;">Reset painting</a>
-</div>
-<canvas id="colors_sketch" width="400" height="400"></canvas>
-</div>
-
-<script type="text/javascript">
-  $(function() {
-    $.each(['#f00', '#ff0', '#0f0', '#0ff', '#00f', '#f0f', '#000', '#fff'], function() {
-      $('#colors_demo .tools').append("<a href='#colors_sketch' data-color='" + this + "' style='width: 10px; background: " + this + ";'></a> ");
-    });
-    $.each([3, 5, 10, 15], function() {
-      $('#colors_demo .tools').append("<a href='#colors_sketch' data-size='" + this + "' style='background: #ccc'>" + this + "</a> ");
-    });
-    $('#colors_sketch').sketch();
-  });
-</script>
-*/ ?>
-
 <p><a style="display:none;" title="Freshalicious" 
       href="http://www.robodesign.ro/marius/my-projects/images/freshalicious"><img 
       id="editableImage" src="<?=erLhcoreClassDesign::design('js/paintweb/bgedit.jpg')?>" alt="Freshalicious"></a></p>
@@ -35,9 +10,8 @@
 
     <script type="text/javascript" src="<?=erLhcoreClassDesign::design('js/paintweb/build/paintweb.js')?>"></script>
 
-    <script type="text/javascript"><!--
+    <script type="text/javascript">
 (function () { 
-  // Function called when the PaintWeb application fires the "appInit" event.
   function pwInit (ev) {
     var initTime = (new Date()).getTime() - timeStart,
         str = 'Demo: Yay, PaintWeb loaded in ' + initTime + ' ms! ' +
@@ -81,8 +55,7 @@
       timeStart = (new Date()).getTime();
       pw.init(pwInit);
 
-})();
-    --></script>
+})();</script>
 
 
 
