a8af1d33e9aa4baba6e8db937a0a8e04715160bd
Remigijus Kiminas <remdex@gmail.com>
2011-11-03 20:43:50 +0000

    IE9 sketch fix

--- design/defaulttheme/tpl/lhsimilar/sketch.tpl.php
+++ design/defaulttheme/tpl/lhsimilar/sketch.tpl.php
@@ -2,19 +2,18 @@
             <h1>Search by sketch</h1>
 </div>
     
-<p><a style="display:none;" title="Freshalicious" 
+<div id="PaintWebTarget"></div>
+    
+<p><a style="position:ansolute;top:-5000px;left:-5000px;"title="Freshalicious" 
       href="http://www.robodesign.ro/marius/my-projects/images/freshalicious"><img 
       id="editableImage" src="<?=erLhcoreClassDesign::design('js/paintweb/bgedit.jpg')?>" alt="Freshalicious"></a></p>
-
-    <div id="PaintWebTarget"></div>
-
+    
     <script type="text/javascript" src="<?=erLhcoreClassDesign::design('js/paintweb/build/paintweb.js')?>"></script>
 
     <script type="text/javascript">
 (function () { 
   function pwInit (ev) {
     var initTime = (new Date()).getTime() - timeStart,str = 'Demo: Yay, PaintWeb loaded in ' + initTime + ' ms! ' + pw.toString();
-    document.body.removeChild(loadp);
     if (ev.state === PaintWeb.INIT_ERROR) {
       alert('Demo: PaintWeb initialization failed.');
       return;
@@ -32,17 +31,21 @@
   };
   var img    = document.getElementById('editableImage'),
       target = document.getElementById('PaintWebTarget'),
-      loadp  = document.createElement('p'),
       timeStart = null,
       pw = new PaintWeb();
       pw.config.guiPlaceholder = target;
       pw.config.imageLoad      = img; 
       pw.config.uploadCanvasURL      = '<?=erLhcoreClassDesign::baseurl('similar/uploadcanvas')?>';
       pw.config.configFile     = 'config-example.json';
-      loadp.appendChild(document.createTextNode('Loading, please wait...'));
       timeStart = (new Date()).getTime();
-      pw.init(pwInit);
+      //setTimeout(function(){ 
+        pw.init(pwInit);
+      //},5000)
+      
 })();</script>
 
+<br />
+
 <div id="img-list-search">
+
 </div>
