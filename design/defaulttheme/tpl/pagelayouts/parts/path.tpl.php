ddf36428b926cbd0d19a5a8605d8e796faf52315
Remigijus Kiminas <remdex@gmail.com>
2011-12-10 07:08:11 +0000

    Breadcrumb update

--- design/defaulttheme/tpl/pagelayouts/parts/path.tpl.php
+++ design/defaulttheme/tpl/pagelayouts/parts/path.tpl.php
@@ -4,6 +4,6 @@ if ($pathElementCount >= 0):
 ?>			
 <ol id="breadcrumb" itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
 <li><a rel="home" itemprop="url" href="<?=erLhcoreClassDesign::baseurl()?>"><span itemprop="title"><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Home')?></span></a></li>
-<? foreach ($Result['path'] as $key => $pathItem) : if (isset($pathItem['url'])) { ?><li itemprop="child" itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="<?=$pathItem['url']?>" itemprop="url"><span itemprop="title"><?=htmlspecialchars($pathItem['title'])?></span></a></li><? } else { ?><li itemprop="child" itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><span itemprop="title"><?=htmlspecialchars($pathItem['title'])?></span></li><? }; ?><? endforeach; ?>
+<? foreach ($Result['path'] as $key => $pathItem) : if (isset($pathItem['url']) && $pathElementCount != $key) { ?><li itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="<?=$pathItem['url']?>" itemprop="url"><span itemprop="title"><?=htmlspecialchars($pathItem['title'])?></span></a></li><? } else { ?><li itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><span itemprop="title"><?=htmlspecialchars($pathItem['title'])?></span></li><? }; ?><? endforeach; ?>
 </ol><? endif; ?>
 <?php endif;?>
