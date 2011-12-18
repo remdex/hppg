ac9e3acec904771141164182088059ece35f807e
Remigijus Kiminas <remdex@gmail.com>
2011-12-18 07:06:25 +0000

    Facet menu

---
+++ design/defaulttheme/tpl/pagelayouts/parts/leftmenu_facet.tpl.php
@@ -0,0 +1,13 @@
+<div class="left-infobox">				
+		<h3><?=erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Refine your search')?></h3>
+		<?php if (isset($Result['face_album_id'])) : ?>
+	    <a class="reset-filter" href="<?=$Result['facet_url']?>">&laquo; Reset album filter &raquo;</a>
+	    <?php endif;?>	
+		    
+		<ul class="facet-albums">		
+		    
+		    <?php foreach ($Result['facet_list'] as $album) : ?>										
+		      <li><a <?=isset($Result['face_album_id']) && $album->aid == $Result['face_album_id'] ? 'class="selected"' : ''?> href="<?=$Result['facet_url']?>/(album)/<?=$album->aid?>"><?=$album->title?> (<?=$Result['facet_data'][$album->aid]?>)</a>                 
+		    <?php endforeach;?>
+		</ul>									
+</div>
