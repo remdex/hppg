5a2ecf7295525fbcd5321c3bc3d8743dee15eba6
Remigijus Kiminas <remdex@gmail.com>
2011-12-25 21:30:52 +0000

    Bugfix

--- design/defaulttheme/tpl/lhgallery/showimageinfo.tpl.php
+++ design/defaulttheme/tpl/lhgallery/showimageinfo.tpl.php
@@ -34,13 +34,16 @@
         $lastID = $pallete->id;
         ?>
         <div style="background-color:rgb(<?=$pallete->red?>,<?=$pallete->green?>,<?=$pallete->blue?>)">
-        <a href="<?=empty($sort) ? erLhcoreClassDesign::baseurl('gallery/color') : erLhcoreClassDesign::baseurl('gallery/search')?><?=(strpos($sort,'/(color)/') !== false ? str_replace('/(color)/','/(color)/'.$pallete->id.'/',$sort) : $sort.'/(color)/'.$pallete->id)?>"></a>
+        <a href="<?=(empty($sort) || strpos($sort,'(keyword)') === false) ? erLhcoreClassDesign::baseurl('gallery/color').(strpos($sort,'/(color)/') !== false ? str_replace('/(color)/','/(color)/'.$pallete->id.'/',$sort) : '/(color)/'.$pallete->id) : erLhcoreClassDesign::baseurl('gallery/search').(strpos($sort,'/(color)/') !== false ? str_replace('/(color)/','/(color)/'.$pallete->id.'/',$sort) : $sort.'/(color)/'.$pallete->id)?>"></a>
         </div>
         <?php endforeach;sort($topThreeColors);?>
         
     </div>
     <?php endif;?>
-    <a title="<?=erTranslationClassLhTranslation::getInstance()->getTranslation('gallery/image','Search for similar colors images')?>" href="<?=empty($sort) ? erLhcoreClassDesign::baseurl('gallery/color') : erLhcoreClassDesign::baseurl('gallery/search')?><?=(strpos($sort,'/(color)/') !== false ? str_replace('/(color)/','/(color)/'.implode('/',$topThreeColors).'/',$sort) : $sort.'/(color)/'.implode('/',$topThreeColors))?>"><img src="<?=erLhcoreClassDesign::design('images/icons/color_wheel.png')?>" alt="" /></a>
+    
+    <a title="<?=erTranslationClassLhTranslation::getInstance()->getTranslation('gallery/image','Search for similar colors images')?>" href="<?=(empty($sort) || strpos($sort,'(keyword)') === false) ? erLhcoreClassDesign::baseurl('gallery/color').(strpos($sort,'/(color)/') !== false ? str_replace('/(color)/','/(color)/'.implode('/',$topThreeColors).'/',$sort) : '/(color)/'.implode('/',$topThreeColors)) : erLhcoreClassDesign::baseurl('gallery/search').(strpos($sort,'/(color)/') !== false ? str_replace('/(color)/','/(color)/'.implode('/',$topThreeColors).'/',$sort) : $sort.'/(color)/'.implode('/',$topThreeColors))?>"><img src="<?=erLhcoreClassDesign::design('images/icons/color_wheel.png')?>" alt="" /></a>
+    
+    
     <?php if (erConfigClassLhConfig::getInstance()->getSetting( 'imgseek', 'enabled' ) === true) : ?>
     &nbsp;<a title="<?=erTranslationClassLhTranslation::getInstance()->getTranslation('gallery/image','Search for visualy similar images')?>" href="<?=erLhcoreClassDesign::baseurl('similar/image')?>/<?=$image->pid?>"><img src="<?=erLhcoreClassDesign::design('images/icons/eye.png')?>" alt="" /></a>
     <?php endif;?>
